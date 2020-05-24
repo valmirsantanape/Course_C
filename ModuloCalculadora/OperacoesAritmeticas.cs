efc08f44bd5693142fd472b917820b02456727d8
Valmir Santana <valmirsantanape@gmail.com>
2020-05-24 02:16:42 -0300

    Modulo calculadora
    Implementação da função DLL

---
+++ ModuloCalculadora/OperacoesAritmeticas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModuloCalculadora
+{
+    public static class OperacoesAritmeticas
+        //Uma classe estática não pode ser instanciada. Não pode herdar e nem pode ser herdada.
+        //A classe estática so pode ter membros estáticos 
+    {
+        public static double Adicao(double parcela1, double parcela2)
+        {
+            return parcela1 + parcela2;
+        }
+        
+        public static double Subtracao(double minuando, double subtraendo)
+        {
+            return minuando - subtraendo;
+        }
+        public static double multiplicacao(double multiplicando, double multiplicador)
+        {
+            return multiplicando * multiplicador;
+        }
+        public static double divisao(double dividendo, double divisor)
+        {
+            return dividendo / divisor;
+
+        }
+    }
+}
---
+++ ModuloCalculadora/Tabuada.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModuloCalculadora
+{
+    public class Tabuada
+    {
+        
+        public int numero { get; }
+
+        public Tabuada(int numero)
+        {
+            this.numero = numero;
+        }
+        public string Calcular(int de, int ate)
+        {
+            var sb = new StringBuilder();
+            for(int i = de; i <= ate; i++)
+            {
+                sb.AppendLine($"{numero} X {i} = {OperacoesAritmeticas.multiplicacao(numero, i)}");
+            }
+            return sb.ToString();
+        }
+    }
+}
